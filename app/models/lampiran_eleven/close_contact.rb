aae3d2692cf7ce46eb7afe256e620ed822fd0c56
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-05-15 11:37:30 +0800

    tes

--- app/models/lampiran_eleven/close_contact.rb
+++ app/models/lampiran_eleven/close_contact.rb
@@ -33,8 +33,8 @@ class LampiranEleven::CloseContact < ApplicationRecord
   # has_one :close_contact_info_home, class_name: 'LampiranEleven::CloseContactInfoHome', foreign_key: :lampiran_eleven_close_contact_id
 
   belongs_to :information_expose, class_name: 'LampiranEleven::InformationExpose', foreign_key: :lampiran_eleven_information_expose_id
-  belongs_to :start_travel_qn_2, class_name: 'Main::City', foreign_key: :start_travel_qn_2_id
-  belongs_to :end_travel_qn_2, class_name: 'Main::City', foreign_key: :end_travel_qn_2_id
-  belongs_to :job_type, class_name: 'Main::JobType', foreign_key: :main_job_type_id
-  belongs_to :transportation, class_name: 'Main::Transportation', foreign_key: :main_transportation_id
+  belongs_to :start_travel_qn_2, class_name: 'Main::City', foreign_key: :start_travel_qn_2_id, optional: true
+  belongs_to :end_travel_qn_2, class_name: 'Main::City', foreign_key: :end_travel_qn_2_id, optional: true
+  belongs_to :job_type, class_name: 'Main::JobType', foreign_key: :main_job_type_id, optional: true
+  belongs_to :transportation, class_name: 'Main::Transportation', foreign_key: :main_transportation_id, optional: true
 end
