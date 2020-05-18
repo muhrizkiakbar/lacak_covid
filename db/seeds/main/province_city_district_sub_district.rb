d3394d6f4ff306fec3548427554fe7bc7af8701b
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-05-19 04:46:06 +0800

    seeder full

--- db/seeds/main/province_city_district_sub_district.rb
+++ db/seeds/main/province_city_district_sub_district.rb
@@ -1,56 +1,54 @@
-# module ProvinceCityDistrictSubDistrict 
+
     puts "Add Province"
-    provinceurl = 'https://dev.farizdotid.com/api/daerahindonesia/provinsi/63'
+    provinceurl = 'https://dev.farizdotid.com/api/daerahindonesia/provinsi'
     #RestClient adalah plugin dengan menggunakan object RestClient untuk mengambil data dari api
     responseprovince = RestClient.get(provinceurl)
     #JSON.parse --> untuk membuat data response menjadi format JSON dan mengambil object semuaprovinsi
-    jsonresponseprovince = JSON.parse(responseprovince)
+    jsonresponseprovince = JSON.parse(responseprovince)["provinsi"]
     # puts jsonresponseprovince
-    # jsonresponseprovince.each do |province|
+    jsonresponseprovince.each do |province|
     puts "===================================="
-    puts jsonresponseprovince
-        addprovince= Main::Province.create(province: jsonresponseprovince["nama"])
+        addprovince= Main::Province.create(province: province["nama"])
 
-        cityurl = 'https://dev.farizdotid.com/api/daerahindonesia/kota?id_provinsi=63'
+        cityurl = 'https://dev.farizdotid.com/api/daerahindonesia/kota?id_provinsi='+province["id"].to_s
         responsecity = RestClient.get(cityurl)
 
         jsonresponsecity = JSON.parse(responsecity)["kota_kabupaten"]
 
         # # puts jsonresponsecity
-        # jsonresponsecity.each do |city|
-        #     addcity = Main::City.new
-        #     addcity.city= city["nama"]
-        #     addcity.province= addprovince
-        #     addcity.save
-        #     idcityapi = city["id"]
+        jsonresponsecity.each do |city|
+            addcity = Main::City.new
+            addcity.city= city["nama"]
+            addcity.province= addprovince
+            addcity.save
+            idcityapi = city["id"]
 
-        #     puts city["nama"]
+            puts city["nama"]
 
 
-        #     kecamatanurl = 'https://dev.farizdotid.com/api/daerahindonesia/kecamatan?id_kota='+idcityapi.to_s
-        #     responsekecamatan = RestClient.get(kecamatanurl)
-        #     jsonresponsekecamatan = JSON.parse(responsekecamatan)["kecamatan"]
-        #     jsonresponsekecamatan.each do |kecamatan|
-        #         addkecamatan = Main::District.new
-        #         addkecamatan.district = kecamatan["nama"]
-        #         addkecamatan.city= addcity
-        #         addkecamatan.save
-        #         idkecamatanapi = kecamatan["id"]
+            kecamatanurl = 'https://dev.farizdotid.com/api/daerahindonesia/kecamatan?id_kota='+idcityapi.to_s
+            responsekecamatan = RestClient.get(kecamatanurl)
+            jsonresponsekecamatan = JSON.parse(responsekecamatan)["kecamatan"]
+            jsonresponsekecamatan.each do |kecamatan|
+                addkecamatan = Main::District.new
+                addkecamatan.district = kecamatan["nama"]
+                addkecamatan.city= addcity
+                addkecamatan.save
+                idkecamatanapi = kecamatan["id"]
 
-        #         puts kecamatan["nama"]
+                puts kecamatan["nama"]
 
-        #         kelurahanurl = 'https://dev.farizdotid.com/api/daerahindonesia/kelurahan?id_kecamatan='+idkecamatanapi.to_s
-        #         responsekelurahan = RestClient.get(kelurahanurl)
-        #         jsonresponsekelurahan = JSON.parse(responsekelurahan)["kelurahan"]
-        #         jsonresponsekelurahan.each do |kelurahan|
-        #             addkelurahan = Main::SubDistrict.new
-        #             addkelurahan.sub_district= kelurahan["nama"]
-        #             addkelurahan.district= addkecamatan
-        #             addkelurahan.save
+                kelurahanurl = 'https://dev.farizdotid.com/api/daerahindonesia/kelurahan?id_kecamatan='+idkecamatanapi.to_s
+                responsekelurahan = RestClient.get(kelurahanurl)
+                jsonresponsekelurahan = JSON.parse(responsekelurahan)["kelurahan"]
+                jsonresponsekelurahan.each do |kelurahan|
+                    addkelurahan = Main::SubDistrict.new
+                    addkelurahan.sub_district= kelurahan["nama"]
+                    addkelurahan.district= addkecamatan
+                    addkelurahan.save
 
-        #             puts kelurahan["nama"]
-        #         end
-        #     end
-        # end
-    # end
-# end
+                    puts kelurahan["nama"]
+                end
+            end
+        end
+    end
