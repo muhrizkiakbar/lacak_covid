5b68bcb807c5c8802a3af5ba4f3bcb1235453818
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-05-18 23:32:03 +0800

    tes

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "lacak_covid"
 set :repo_url, "git@github.com:muhrizkiakbar/lacak_covid.git"
 
 # 
-set :deploy_to, '/home/deploy/lacak_covid'
+set :deploy_to, "/home/deploy/#{fetch :application}"
 
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
