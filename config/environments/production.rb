eea6be5a6465f18405e5a00b493244114eb62582
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-05-18 20:52:28 +0800

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,11 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
+  config.telegram_updates_controller.session_store = :file_store
+
+
+  routes.default_url_options = {host: 'lacakcovid.kalselprov.go.id', protocol: 'https'} # https is necessary!
+  
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
