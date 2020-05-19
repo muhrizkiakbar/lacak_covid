3fd06852fff23b233b22a7b559c795c9910570d1
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-05-20 06:05:39 +0800

    tes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,7 +13,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf)
+# Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf)
 Rails.application.config.assets.precompile += %w( application.css application.js )
 Rails.application.config.assets.precompile += %w( argon/argon.js )
 Rails.application.config.assets.precompile += %w( argon/demo.js )
