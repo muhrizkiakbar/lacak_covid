4adece4ec33afc753924fcc93863938457512c91
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-05-19 10:43:42 +0800

    tes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,8 @@ Rails.application.configure do
 
   routes.default_url_options = {host: 'lacakcovid.kalselprov.go.id', protocol: 'https'} # https is necessary!
   
+  config.assets.compile = true
+  config.serve_static_assets = true
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
@@ -33,7 +35,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
