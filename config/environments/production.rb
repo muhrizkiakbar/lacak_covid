eeb4dada8d518455bfbf60e94c3a67bdae9f5993
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-05-20 11:50:06 +0800

    tes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # routes.default_url_options = {host: 'lacakcovid.kalselprov.go.id', protocol: 'https'} # https is necessary!
   
   config.assets.compile = true
-  # config.serve_static_assets = true
+  config.serve_static_assets = true
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = true
