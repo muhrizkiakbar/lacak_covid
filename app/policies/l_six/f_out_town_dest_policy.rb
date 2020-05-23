3632d14e93d76381f97b3c375f47b160719890be
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-05-23 13:11:48 +0800

    tes

---
+++ app/policies/l_six/f_out_town_dest_policy.rb
@@ -0,0 +1,32 @@
+class LSix::FOurTownDestPolicy < ApplicationPolicy
+    def index?
+      check_access("l_six_f_our_town_dest","index")
+    end
+  
+    def create?
+        check_access("l_six_f_our_town_dest","create")
+    end
+  
+    def new?
+        check_access("l_six_f_our_town_dest","new")
+    end
+  
+    def show?
+        check_access("l_six_f_our_town_dest","show")
+    end
+  
+    def update?
+        check_access("l_six_f_our_town_dest","edit")
+    end
+  
+    def destroy?
+        check_access("l_six_f_our_town_dest","destroy")
+    end
+  
+    private
+  
+    def check_access resource, action
+        RolePermission.check_status(user.role,Permission.check_resource_and_action(resource,action))
+    end
+  end
+  
