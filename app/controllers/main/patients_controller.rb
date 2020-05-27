19d3f52508a36c8b588286f66db0ccbb37f39ba8
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-05-27 17:48:02 +0800

    push pencarian

--- app/controllers/main/patients_controller.rb
+++ app/controllers/main/patients_controller.rb
@@ -4,10 +4,104 @@ class Main::PatientsController < ApplicationController
   # GET /main/patients
   # GET /main/patients.json
   def index
-    # @main_patients = Main::Patient.
+    if (current_user.role.is_dinkes_region) || (current_user.role.is_public_health_center)
+      if (current_user.role.is_dinkes_region)
+        @main_patients = Main::Patient.where(main_city_id: current_user.dinkes_region.city.id)
+      else
+        @main_patients = Main::Patient.where(main_public_health_center_id: current_user.public_health_center.id)
+      end
+    else
+      @main_patients = Main::Patient.all
+    end
 
-    if !(params[:main_patient][:no_identity]).blank?
-      @main_patients = @main_patients.where(no_identity: params[:main_patient][:no_identity])
+    if !(params[:no_identity].blank?)
+      @no_identity = params[:no_identity]
+      @main_patients = @main_patients.where(no_identity: params[:no_identity])
+    else
+      @no_identity = nil
+    end
+
+    if !(params[:name].blank?)
+      @name = params[:name]
+      @main_patients = @main_patients.where("name LIKE :name", name: "%#{@name}%")
+
+    else
+      @name = nil
+    end
+
+    if !(params[:date_of_birth].blank?)
+      @date_of_birth = params[:date_of_birth]
+      @main_patients = @main_patients.where("date_of_birth LIKE :date_of_birth", date_of_birth: "%#{@date_of_birth}%")
+    else
+      @date_of_birth = nil
+    end
+
+    if !(params[:main_city_id].blank?)
+      @main_city = Main::City.friendly.find(params[:main_city_id])
+      @main_patients = @main_patients.where(main_city_id: @main_city)
+    else
+      @main_city = nil
+    end
+
+    if !(params[:main_district_id].blank?)
+      @main_district = Main::District.friendly.find(params[:main_district_id])
+      @main_patients = @main_patients.where(main_district_id: @main_district)
+    else
+      @main_district = nil
+    end
+
+    if !(params[:main_sub_district_id].blank?)
+      @main_sub_district = Main::SubDistrict.friendly.find(params[:main_sub_district_id])
+      @main_patients = @main_patients.where(main_sub_district_id: @main_sub_district)
+    else
+      @main_sub_district = nil
+    end
+
+    if !(params[:main_citizen_association_id].blank?)
+      @main_citizen_association = Main::CitizenAssociation.friendly.find(params[:main_citizen_association_id])
+      @main_patients = @main_patients.where(main_citizen_association_id: @main_citizen_association)
+    else
+      @main_citizen_association = nil 
+    end
+
+    if !(params[:main_neighborhood_association_id].blank?)
+      @main_neighborhood_association = Main::NeighborhoodAssociation.friendly.find(params[:main_neighborhood_association_id])
+      @main_patients = @main_patients.where(main_neighborhood_association_id: @main_neighborhood_association)
+    else
+      @main_neighborhood_association = nil       
+    end
+
+    if !(params[:main_marital_status_id].blank?)
+
+      @main_marital_status = Main::MaritalStatus.friendly.find(params[:main_marital_status_id])
+      @main_patients = @main_patients.where(main_marital_status_id: @main_marital_status)
+    else
+      @main_marital_status = nil       
+      
+    end
+
+    if !(params[:name_of_parent].blank?)
+     
+      @name_of_parent = params[:name_of_parent]
+      @main_patients = @main_patients.where("name_of_parent LIKE :name_of_parent", name_of_parent: "%#{@name_of_parent}%")
+    else
+      @name_of_parent = nil    
+    end
+
+    if !(params[:phone_number].blank?)
+      
+      @phone_number = params[:phone_number]
+      @main_patients = @main_patients.where("phone_number LIKE :phone_number", phone_number: "%#{@phone_number}%")
+    else
+      @phone_number = nil    
+    end
+
+    if !(params[:gender].blank?)
+      @gender = params[:gender]
+      @main_patients = @main_patients.where(gender: @gender)
+    else
+      @gender = nil   
+      
     end
 
     @main_patients = @main_patients.page params[:page]
