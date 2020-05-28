e9a40a10528e9702ccc8c2ef4f3ac1b8fe3e9e6f
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-05-28 22:56:58 +0800

    tes

--- app/models/telegram/username_observer.rb
+++ app/models/telegram/username_observer.rb
@@ -47,4 +47,8 @@ class Telegram::UsernameObserver < ApplicationRecord
   belongs_to :public_health_center, class_name: 'Main::PublicHealthCenter', foreign_key: :main_public_health_center_id, optional: true
   belongs_to :user, class_name: 'User', foreign_key: :user_id
 
+  def user
+    User.unscoped {super}
+  end
+
 end
