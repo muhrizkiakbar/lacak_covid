af87d2e254e581486e93a24e51052e251691f88e
Nat4Lia <33526078+Nat4Lia@users.noreply.github.com>
2020-05-30 05:35:33 +0800

    error notifikasi login

--- vendor/assets/javascripts/argon/custom.js
+++ vendor/assets/javascripts/argon/custom.js
@@ -36,6 +36,41 @@ $('div.notifikasi').show(function(){
     $(this).hide();
 });
 
+$('div.sessions-notification').show(function(){
+    notifMessage = $(this).data('notifikasi');
+    notifType = ( $(this).data('type') == 'alert')? 'danger' : 'success';
+    $.notify({
+        icon: "ni ni-bell-55",
+        title: notifMessage,
+        message: "",
+        url: ""
+    }, {
+        element: "body",
+        type: notifType,
+        allow_dismiss: true,
+        placement: {
+            from: "top",
+            align: "center"
+        },
+        offset: {
+            x: 15,
+            y: 15
+        },
+        spacing: 10,
+        z_index: 1080,
+        delay: 3000,
+        timer: 1000,
+        url_target: "_blank",
+        mouse_over: !1,
+        animate: {
+            enter: "animated fadeInDown",
+            exit: "animated fadeOutUp"
+        },
+        template: '<div data-notify="container" class="alert alert-dismissible alert-{0} alert-notify" role="alert"><span class="alert-icon" data-notify="icon"></span> <div class="alert-text"</div> <span class="alert-title" data-notify="title">{1}</span> <span data-notify="message">{2}</span></div><button type="button" class="close" data-notify="dismiss" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'
+    });
+    $(this).hide();
+});
+
 var enableDatepicker = function () {
     $('.datepicker').datepicker({
         format : 'yyyy-mm-dd',
