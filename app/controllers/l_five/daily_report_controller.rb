f2bddbc85ebba568a0a2a1507fe8e92b03bea471
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-06-03 02:22:57 +0800

    tes

--- app/controllers/l_five/daily_report_controller.rb
+++ app/controllers/l_five/daily_report_controller.rb
@@ -34,14 +34,14 @@ class LFive::DailyReportController < ApplicationController
 
                                 l_six = LSix::First.where(main_patient_id: patient)
 
-                                if params[:l_five_daily_report][:date].blank? || params[:l_five_daily_report][:date].nil? || params[:l_five_daily_report][:date] == ""
-                                    l_six = l_six.this_day
-                                    @date = Date.today
-                                else
-                                    date_params = params[:l_five_daily_report][:date]
-                                    l_six = l_six.where(interview_date: date_params)
-                                    @date = date_params
-                                end
+                                # if params[:l_five_daily_report][:date].blank? || params[:l_five_daily_report][:date].nil? || params[:l_five_daily_report][:date] == ""
+                                #     l_six = l_six.this_day
+                                #     @date = Date.today
+                                # else
+                                #     date_params = params[:l_five_daily_report][:date]
+                                #     l_six = l_six.where(interview_date: date_params)
+                                #     @date = date_params
+                                # end
                                 
                                 count_confirm = l_six.count_kk
                                 count_odp = l_six.count_odp
@@ -99,14 +99,14 @@ class LFive::DailyReportController < ApplicationController
 
                     l_six = LSix::First.where(main_patient_id: patient.id)
 
-                    if params[:l_five_daily_report][:date].blank? || params[:l_five_daily_report][:date].nil? || params[:l_five_daily_report][:date] == ""
-                        l_six = l_six.this_day
-                        @date = Date.today
-                    else
-                        date_params = params[:l_five_daily_report][:date]
-                        l_six = l_six.where(interview_date: date_params)
-                        @date = date_params
-                    end
+                    # if params[:l_five_daily_report][:date].blank? || params[:l_five_daily_report][:date].nil? || params[:l_five_daily_report][:date] == ""
+                    #     l_six = l_six.this_day
+                    #     @date = Date.today
+                    # else
+                    #     date_params = params[:l_five_daily_report][:date]
+                    #     l_six = l_six.where(interview_date: date_params)
+                    #     @date = date_params
+                    # end
                     
                     count_confirm = l_six.count_kk
                     count_odp = l_six.count_odp
