149ab0fa3fbc86d95b25669af70cec34a751cd4b
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-06-02 23:12:50 +0800

    tes

--- db/migrate/20200602005233_add_index_email_to_user.rb
+++ db/migrate/20200602005233_add_index_email_to_user.rb
@@ -1,7 +1,7 @@
 class AddIndexEmailToUser < ActiveRecord::Migration[5.2]
   def change
 
-    remove_index :users, name: "index_users_on_email"
+    # remove_index :users, name: "index_users_on_email"
     add_index :users, :email,                unique: true
   end
 end
