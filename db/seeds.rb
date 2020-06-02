e3e918e155e7abc60c67676aba528d48b09c8924
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-06-02 22:53:16 +0800

    tes

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,19 @@
 #     load seed
 # end
 
+puts "Croscheck user"
+users = User.with_deleted.all
+
+users.each_with_index do |user,key|
+    checkuser = User.where(email: user.email).count
+    if checkuser > 1
+        update = User.find(user.id)
+        update.email = update.email.to_s + key.to_s
+        update.username = update.username.to_s + key.to_s
+        update.save
+    end
+end
+
 Dir[File.join(Rails.root, 'db', 'seeds/policy/l_five', '*.rb')].sort.each do |seed|
     load seed
 end
@@ -35,3 +48,4 @@ end
 # Dir[File.join(Rails.root, 'db', 'seeds', 'user.rb')].sort.each do |seed|
 #     load seed
 # end
+
