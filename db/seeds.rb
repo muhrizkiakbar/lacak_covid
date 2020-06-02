ab23dff3e94d81f81ee71fa87451f56791b18a89
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-06-02 23:22:58 +0800

    tes

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,13 +23,13 @@ users = User.only_deleted.order("created_at asc").all
 
 users.each_with_index do |user,key|
     checkuser = User.where(email: user.email).only_deleted.count
-    if checkuser > 1
+    # if checkuser > 1
 
         update = User.find(user.id)
         update.email = update.email.to_s + key.to_s
         update.username = update.username.to_s + key.to_s
         update.save
-    end
+    # end
 end
 
 Dir[File.join(Rails.root, 'db', 'seeds/policy/l_five', '*.rb')].sort.each do |seed|
